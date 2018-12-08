0c967e141fcdf10e13af5b342fdfb4074e55783a
Aaron Grey <aarongrey@Aarons-MBP.lan>
2018-12-08 08:55:56 -0800

    Under construction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/img/logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div>
+          <img src={logo} className='App-logo' alt='logo' />
+        </div>
+        <div className='welcome-message'>
+          <h1>Troop 2119</h1>
+          <h2>
+            Please visit us on Facebook
+            <br /> while we work on our new web site.
+            <br />
+            <br />
+            <a href='https://www.facebook.com/groups/troop2119/'>
+              Troop 2119 Facebook Group
+            </a>
+          </h2>
+        </div>
       </div>
     );
   }
